70829af8a12378e0906cb8630caeaa9063485b1f
JagjotB <jagjot500@gmail.com>
2025-12-17 13:51:57 -0500

    updating breakfast.py

--- breakfast.py
+++ breakfast.py
@@ -1,5 +1,7 @@
-response = input("Please place your order. Would you like waffles or pancakes?\n")
+response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
 if response == "waffles":
     print("Waffles it is!")
 elif response == "pancakes":
     print("Pancakes it is!")
+else:
+    print("Sorry, I don't understand.")
