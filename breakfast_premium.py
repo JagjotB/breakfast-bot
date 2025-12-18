5747812c0ac45e447cc49cb9779c466c57ff3efc
JagjotB <jagjot500@gmail.com>
2025-12-18 17:02:35 -0500

    added pauses

--- breakfast_premium.py
+++ breakfast_premium.py
@@ -1,16 +1,25 @@
-print("Hello! I am Bob, the Breakfast Bot. \n"
-      "Today we have two breakfast options available. \n"
-      "The firt is waffles with strawberries and whipped cream. \n" 
-      "The second is pancakes with butter and syrup. \n")
+import time
+
+print("Hello! I am Bob, the Breakfast Bot.")
+time.sleep(1)
+print("Today we have two breakfast options available.")
+time.sleep(1)
+print("The firt is waffles with strawberries and whipped cream.") 
+time.sleep(1)
+print("The second is pancakes with butter and syrup.")
+time.sleep(1)
 while True:
     response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
     if "waffles" in response:
         print("Waffles it is!")
+        time.sleep(1)
         break
     elif "pancakes" in response:
-        print("Pancakes it is! \n Your order will  be ready shortly")
+        print("Pancakes it is!")
+        time.sleep(1)
         break
     else:
         print("Sorry, that's not on our menu.")
+        time.sleep(1)
 
 print("Your order will be ready shortly.")
