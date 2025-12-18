b3ba1060ca2a9571a3079e7dfe950dc09cd28f5a
JagjotB <jagjot500@gmail.com>
2025-12-18 16:22:54 -0500

    pushing changes to breakfast.py

--- breakfast.py
+++ breakfast.py
@@ -1,7 +1,10 @@
-response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
-if response == "waffles":
-    print("Waffles it is!")
-elif response == "pancakes":
-    print("Pancakes it is!")
-else:
-    print("Sorry, I don't understand.")
+while True:
+    response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
+    if "waffles" in response:
+        print("Waffles it is!")
+        break
+    elif "pancakes" in response:
+        print("Pancakes it is!")
+        break
+    else:
+        print("Sorry, that's not on our menu.")
